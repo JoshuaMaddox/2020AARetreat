2103895a72368b5e63c7091ae59c8b3e2051243e
Joshua Maddox <joshua@cinderhousecreative.com>
2020-12-09 23:13:17 -0800

    fixed loader

--- src/pages/form.js
+++ src/pages/form.js
@@ -4,14 +4,14 @@ import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
 import LoomOne from '../images/loom_one.png'
+import loader from "../images/loading.gif"
 
 function Form() {
   return (
     <Layout>
       <SEO title="The Things Form — Acumen Academy Retreat" />
       <div className="main-message">
-     
-        <iframe className="airtable-embed" src="https://airtable.com/embed/shrLuj6HpJvObSiXM?backgroundColor=pink" frameBorder="0" width="100%" height="1200" style={{background: 'transparent', border: '1px solid #ccc'}} ></iframe>
+        <iframe className="airtable-embed" src="https://airtable.com/embed/shrLuj6HpJvObSiXM?backgroundColor=pink" frameBorder="0" width="100%" height="1200" style={{background: `url(${loader}) center center no-repeat`, border: 'none'}} ></iframe>
 </div>
       
     </Layout>
